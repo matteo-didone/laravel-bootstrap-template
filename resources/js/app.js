6ec90a0dd931db48f9ecd1d05a913f23b14b256a
matteo-didone <matteo.dido9@gmail.com>
2023-08-31 15:59:28 +0200

    Followed along with tutorial

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import '~resources/scss/app.scss';
+
+import * as bootstrap from 'bootstrap';
+
+import.meta.glob([
+    // Sto dicendo "Se metto dei file, dentro le risorse, che sono img,   ricordati di considerarli"
+    '../img/**',
+]);
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+
+    resolve: {
+        // Che cos'è un alias?
+        // Quando io chiamo qualcosa da qualche parte, in realtà io posso riferirmi a qualcos'altro
+        alias: {
+            //When I say "~resources" I mean "/resources/"
+            '~resources': '/resources/',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        },
+    },
 });
