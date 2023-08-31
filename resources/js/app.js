ba91f059718cd7a3ba7d9469809ac244df562257
matteo-didone <matteo.dido9@gmail.com>
2023-08-31 16:43:34 +0200

    Following along

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,4 @@
 import './bootstrap';
-
-import '~resources/scss/app.scss';
-
 import * as bootstrap from 'bootstrap';
 
 import.meta.glob([
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -12,10 +13,6 @@ export default defineConfig({
     resolve: {
         // Che cos'è un alias?
         // Quando io chiamo qualcosa da qualche parte, in realtà io posso riferirmi a qualcos'altro
-        alias: {
-            //When I say "~resources" I mean "/resources/"
-            '~resources': '/resources/',
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        },
+        
     },
 });
